93bece91dab45125c8f895ab8a491a0bcf1964f9
julcalde <julcalde@student.42heilbronn.de>
2025-09-27 16:02:17 +0200

    ex00: main function to test functionality of the scalarconverter function. Works.

---
+++ ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/27 15:55:31 by julcalde          #+#    #+#             */
+/*   Updated: 2025/09/27 15:57:40 by julcalde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConversion.hpp"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Usage: ./scalarconverter <literal>" << std::endl;
+		return (1);
+	}
+	ScalarConversion::convert(argv[1]);
+	return (0);
+}
