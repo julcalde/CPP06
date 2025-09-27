2c73ffd38419ccfaed6dc4050c0a72673542f25d
julcalde <julcalde@student.42heilbronn.de>
2025-09-27 14:59:59 +0200

    ex00 header file done

---
+++ ex00/ScalarConversion.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConversion.hpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/27 14:50:57 by julcalde          #+#    #+#             */
+/*   Updated: 2025/09/27 14:58:54 by julcalde         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERSION_HPP
+# define SCALARCONVERSION_HPP
+
+# include <string>
+
+class ScalarConversion
+{
+	private:
+		ScalarConversion();
+		~ScalarConversion();
+		ScalarConversion(const ScalarConversion& other);
+		ScalarConversion& operator=(const ScalarConversion& other);
+		
+	public:
+		static void convert(const std::string& input);
+};
+
+#endif
