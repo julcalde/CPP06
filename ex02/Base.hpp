0e2b298a1ead635f153a1d9c30626d7b5705c05e
julcalde <julcalde@student.42heilbronn.de>
2025-09-27 16:52:51 +0200

    ex02: header file of all classes, done

--- ex02/Base.hpp
+++ ex02/Base.hpp
@@ -6,7 +6,28 @@
 /*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 16:45:44 by julcalde          #+#    #+#             */
-/*   Updated: 2025/09/27 16:45:45 by julcalde         ###   ########.fr       */
+/*   Updated: 2025/09/27 16:52:10 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BASE_HPP
+# define BASE_HPP
+
+// Base class with virtual destructor
+class Base
+{
+	public:
+		virtual ~Base();
+};
+
+// Derived classes
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+// Function prototypes for generating and identifying objects of derived classes
+Base *generate(void);
+void identify(Base *p);
+void identify(Base &p);
+
+#endif
